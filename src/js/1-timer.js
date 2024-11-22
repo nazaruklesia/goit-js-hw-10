af97565ec7b233a8fea4af163484fc881866be12
Lesia_Nazaruk <nazarullesia@gmail.com>
2024-11-22 22:39:53 +0200

    update time

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -12,6 +12,12 @@ import "izitoast/dist/css/iziToast.min.css";
 
 
 
+const datetimePicker = document.querySelector("#datetime-picker");
+const btnStart = document.querySelector("button[data-start]");
+
+let userSelectedDate = null;
+
+
 
 const options = {
   enableTime: true,
@@ -19,8 +25,51 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date) {
+      window.alert("Please choose a date in the future");
+      btnStart.disabled = true;
+    }
+    else {
+      btnStart.disabled = false;
+      userSelectedDate = selectedDate;
+    }
   },
 };
+flatpickr(datetimePicker, options);
 
 
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+const daysData = document.querySelector("[days-data]");
+const hoursData = document.querySelector("[data-hours]");
+const minutesData = document.querySelector("[data-minutes]");
+const secondsData = document.querySelector("[data-seconds]");
+
+function updateTime({ days, hours, minutes, seconds }) {
+  daysData.textContent = addLeadingZero(days);
+  hoursData.textContent = addLeadingZero(hours);
+  minutesData.textContent = addLeadingZero(minutes);
+  secondsData.textContent = addLeadingZero(seconds);
+}
